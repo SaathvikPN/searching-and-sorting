80a58abea2708bbdacf68be1d82a9d1d632ac1b3
Saathvik PN <66002135+SaathvikPN@users.noreply.github.com>
2020-07-21 10:34:27 +0530

    Create binary-search.c

---
+++ binary-search.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include<stdlib.h>
+void search(int n, int data[], int first, int last);
+int main ()
+{
+    int data[] = {5,10,15,20,25};
+    int n;
+    printf("Number : ");
+    scanf("%d",&n);
+    
+    search(n,data,0,4);
+    
+}
+
+void search(int n, int data[], int first, int last)
+{
+    while(first<=last)
+    {
+    int mid = (first+last)/2;
+    if(data[mid]==n)
+    {
+        printf("Result : %d",mid);
+        exit(0);
+    }
+    
+    if (data[mid]<n)
+    {
+        first = mid + 1;
+        search(n,data,first,last);
+    }
+    
+    if (data[mid]>n)
+    {
+        last = mid - 1;
+        search(n,data,first,last);
+    }
+    }
+    
+    if(first>last)
+    {
+        printf("Not found\n");
+        exit(0);
+    }   
+}
